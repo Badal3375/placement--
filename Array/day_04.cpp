db1ef6ead4ab08adaad25e63da3408af0e8c6640
Badal Singh <singh.badal3375@gmail.com>
2025-07-15 17:23:34 +0530

    day_4

--- Array/day_04.cpp
+++ Array/day_04.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+// find the  diffrence  between max and min of array  
 int Max_Min(int arr[],int n){
     int max=0 ;
     int min =0;
@@ -12,17 +13,33 @@ int Max_Min(int arr[],int n){
        
     }
      
-    //if(max>min){
-    //    return max-min;
-    //}
-    //else{
-    //    return min-max;
-    //}
+     
       return abs(max-min);
+ }
+// find the fizzbizz problems in using conditinal statement of c++
+void fizzbuzz_program(int n){
+     
+        if(n%3==0||n%5==0){
+        cout<<"FizzBizz"<<endl;
+    }
+    else if(n%3==0||n%5!=0){
+        cout<<"Fizz"<<endl;
+
+    }
+    else if(n%3!=0||n%5==0){
+        cout<<"Bizz"<<endl;
+    }
+    else{
+        cout<<n<<endl;
+    }
+    
 }
 int main(){
+    int n;
+    cin>>n;
+    fizzbuzz_program(n);
     int arr[]={11,2,4,5,10};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int ans=Max_Min(arr,n);
-    cout<<ans<<endl;
+  int n=sizeof(arr)/sizeof(arr[0]);
+  int ans=Max_Min(arr,n);
+  cout<<ans<<endl;
 }
