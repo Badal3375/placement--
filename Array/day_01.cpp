677f222a0e83104aa016d46cc5ad337bef131eb8
Badal Singh <singh.badal3375@gmail.com>
2025-07-16 14:55:48 +0530

    day_05

--- Array/day_01.cpp
+++ Array/day_01.cpp
@@ -3,15 +3,19 @@ using namespace std;
 int max(int *arr,int n){
    int max = arr[0];
    for(int i=0;i<n;i++){
-      if(max<arr[i]){
+      if(arr[i]>max){
          max = arr[i];
+ 
       }
-      return max;
+       
    }
+    return max;
 }
+ 
 int main(){
    int n;
    int arr[]={1,5,6,8,7,9};
    int ans=max(arr,n);
-   cout<<ans;
+   cout<<ans<<endl;
+   
 }
