eb3c3f00aedb20ef99ffbfd430e416a542fa0763
Badal Singh <singh.badal3375@gmail.com>
2025-07-28 00:05:20 +0530

    day_02

--- Array/MYSQL/sql_test.sql
+++ Array/MYSQL/sql_test.sql
@@ -1,2 +1,13 @@
 use bds;
 SHOW tables;
+create database test_1;
+use test_1;
+show DATABASES;
+use test_1;
+create table customer(
+     cust_id INT   PRIMARY KEY AUTO_INCREMENT,
+     cust_name VARCHAR(255),
+     cust_email VARCHAR(255)
+)
+
+desc customer;
