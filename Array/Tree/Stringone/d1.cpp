cc3bee3d905b017a5c5bdb2857609442c28128d7
Badal Singh <singh.badal3375@gmail.com>
2025-08-18 19:44:48 +0530

    day1

---
+++ Array/Tree/Stringone/d1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+    string s="badal";
+    cout<<"  string "<<s<<endl;
+       int start=0;
+     int end=s.size()-1;
+      while(start<end){
+         swap(s[start],s[end]);
+         start++;
+            end--;
+      }
+       cout<<"  string "<<s<<endl;
+}
